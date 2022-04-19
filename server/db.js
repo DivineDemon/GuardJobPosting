355b9384f56d245a4e322474de33b3f8c25dc69d
DivineDemon <supame123@gmail.com>
2022-04-20 00:04:16 +0400

    Boilerplate with MySQL Connection Done :tada:

---
+++ server/db.js
@@ -0,0 +1,9 @@
+import mysql from "mysql";
+
+export const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "guardJobPosting",
+  multipleStatements: true,
+});
---
+++ server/index.js
@@ -0,0 +1,32 @@
+import mysql from "mysql";
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { connection } from "./db.js";
+
+// Importing Routes
+import { router as guardRoute } from "./routes/guard.js";
+
+// Initializing Express App
+const app = express();
+
+// Middleware
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// DB Connect
+connection.connect((err) => {
+  if (!err) {
+    console.log("MySQL DB Connected!");
+  } else {
+    console.log(err);
+  }
+});
+
+// Routes
+app.use("/guards", guardRoute);
+
+// Starting the App
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`));

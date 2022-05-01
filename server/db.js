721a46ec3dcf583041c9fa64d6d310f271f478fc
DivineDemon <supame123@gmail.com>
2022-05-01 23:26:53 +0400

    Updated DB Parameters to ClearDB ✨

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql";
 
 export const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "guardJobPosting",
+  host: "us-cdbr-east-05.cleardb.net",
+  user: "b777740e0c5b5e",
+  password: "e975f755",
+  database: "heroku_d11cc74f6c0497d",
   multipleStatements: true,
 });
 

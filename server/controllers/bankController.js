494b7b84eff74d37cbdaa307206fabbaa210328c
DivineDemon <supame123@gmail.com>
2022-05-22 20:23:23 +0400

    Added Pagination to Get All Banks!✨

--- server/controllers/bankController.js
+++ server/controllers/bankController.js
@@ -1,12 +1,39 @@
 import { connection } from "../db.js";
+const rowsPerPage = process.env.ROWS || 25;
 
 export const getBanks = (req, res) => {
   connection.query("SELECT * FROM bank", (err, rows) => {
-    if (!err) {
-      res.status(201).json(rows);
-    } else {
+    if (err) {
       res.status(500).json(err);
     }
+
+    const numOfRows = rows.length;
+    const numOfPages = Math.ceil(numOfRows / rowsPerPage);
+    let page = req.query.page ? Number(req.query.page) : 1;
+    if (page > numOfPages) {
+      res.send("/?page=" + encodeURIComponent(numOfPages));
+    } else if (page < 1) {
+      res.send("/?page=" + encodeURIComponent("1"));
+    }
+
+    const startingLimit = (page - 1) * rowsPerPage;
+    connection.query(
+      `SELECT * FROM bank LIMIT ${startingLimit}, ${rowsPerPage}`,
+      (err, rows) => {
+        if (err) {
+          res.status(500).json(err);
+        }
+
+        let iterator = page - 5 < 1 ? 1 : page - 5;
+        let endingLink =
+          iterator + 9 <= numOfPages ? iterator + 9 : page + numOfPages;
+        if (endingLink < page + 4) {
+          iterator -= page + 4 - numOfPages;
+        }
+
+        res.send(rows);
+      }
+    );
   });
 };
 
@@ -26,7 +53,7 @@ export const getBank = (req, res) => {
 export const addBank = (req, res) => {
   const { name, accountTitle, accountNo, bsb, abn } = req.body;
   connection.query(
-    `INSERT INTO bank (name, accountTitle, accountNo, bsb, abn) VALUES ('${name}', '${accountTitle}', ${accountNo}, ${bsb}, ${abn})`,
+    `INSERT INTO bank (bankName, accountTitle, accountNo, bsb, abn) VALUES ('${name}', '${accountTitle}', ${accountNo}, ${bsb}, ${abn})`,
     (err, rows, fields) => {
       if (!err) {
         res

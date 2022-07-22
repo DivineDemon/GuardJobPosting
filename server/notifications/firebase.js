17b384bf35306be091ec00872d7cdb4f3b1dc210
DivineDemon <supame123@gmail.com>
2022-07-22 22:34:54 +0400

    :fire: :hammer:

--- server/notifications/firebase.js
+++ server/notifications/firebase.js
@@ -1,5 +1,18 @@
 const { initializeApp, messaging } = require("firebase-admin");
-const serviceAccount = require("./serviceAccount.json");
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: "koala-base",
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: "firebase-adminsdk-4owlv@koala-base.iam.gserviceaccount.com",
+  client_id: "105829812696654302044",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-4owlv%40koala-base.iam.gserviceaccount.com",
+};
 
 const sendNotification = (message) => {
   // Initializing Notification Engine

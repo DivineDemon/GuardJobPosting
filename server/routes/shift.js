2aa1e5b108e9b81e0fa0530bffecf6938c6c8945
DivineDemon <supame123@gmail.com>
2022-07-22 20:32:38 +0400

    :hammer:

--- server/routes/shift.js
+++ server/routes/shift.js
@@ -22,7 +22,7 @@ router.route("/:id").delete(verifyTokenAndCompany, deleteShift).get(getShift);
 router
   .route("/company/:company_id")
   .get(verifyTokenAndCompany, getAppliedShifts);
-router.route("/guard/:guard_id").get(getGuardShifts);
+router.route("/guard/:guard_id").post(getGuardShifts);
 router.get("/job/:job_id", getJobShifts);
 router.post("/job/:job_id/:company_id", verifyTokenAndCompany, addShift);
 router
@@ -31,6 +31,6 @@ router
 router.route("/apply/:job_id/:guard_id").post(verifyTokenAndGuard, applyShift);
 router.put("/approve/guard/:guard_id", verifyTokenAndCompany, approveShifts);
 router.get("/approved", getApprovedShifts);
-router.get("/company/approved/:company_id", getCompanyShifts);
+router.post("/company/approved/:company_id", getCompanyShifts);
 
 module.exports = router;

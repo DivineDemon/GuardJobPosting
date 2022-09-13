c73d2e70b2112478347967462cf6f8adc07b7ea7
DivineDemon <supame123@gmail.com>
2022-09-13 21:12:54 +0400

    :hammer:

--- server/routes/otherDocs.js
+++ server/routes/otherDocs.js
@@ -14,7 +14,7 @@ const router = express.Router();
 
 router.route("/").get(verifyTokenAndAdmin, getOtherdocs);
 router.route("/:id").delete(verifyTokenAndGuard, deleteOtherdocs);
-router.route("/add/:guard_id").post(verifyTokenAndGuard, addOtherdocs);
+router.route("/add/:guard_id").post(addOtherdocs);
 router
   .route("/:document_id/:guard_id")
   .patch(verifyTokenAndGuard, updateOtherdocs);
